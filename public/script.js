4f151fb36667e938df92c4c6c97653dd1d4752ba
RJ Dabbar <rjdabbar@gmail.com>
2015-11-18 14:56:11 -0500

    selecting detail view is more responsive

--- public/script.js
+++ public/script.js
@@ -1,14 +1,21 @@
 $.MovieSearch = function (el) {
   this.$el = $(el);
   this.$form = this.$el.find("form");
-  this.$ul = this.$el.find("ul")
+  this.$results = this.$el.find("ul#results")
   this.$details = this.$el.find("div#details")
   this.bindEvents();
 }
 
 $.MovieSearch.prototype.bindEvents = function () {
   this.$form.on("submit", this.search.bind(this));
-  this.$el.on("click", "li.movie", this.detailSearch.bind(this))
+  this.$el.on("click", "a.showDetails", this.detailSearch.bind(this))
+  this.$el.on("click", "a.showDetails", this.toggleActive.bind(this))
+};
+
+$.MovieSearch.prototype.toggleActive = function (e) {
+  e.preventDefault();
+  this.$results.children().removeClass("active");
+  $(e.currentTarget).parent().addClass("active");
 };
 
 $.MovieSearch.prototype.search = function (e) {
@@ -26,7 +33,7 @@ $.MovieSearch.prototype.search = function (e) {
 };
 
 $.MovieSearch.prototype.renderResults = function (data) {
-  this.$ul.empty();
+  this.$results.empty();
   var results = data["Search"];
   results.forEach(this.generateResult.bind(this));
 };
@@ -35,26 +42,27 @@ $.MovieSearch.prototype.generateResult = function (movie) {
   var title, year;
   title = movie["Title"];
   year = movie["Year"];
-  var $content = $("<div>").html("<h3>" + title + "</h3><h4>Released in: " + year + "</h4>");
-  var $li = $("<li>").data("id", movie["imdbID"]).addClass("movie").append($content)
-  this.$ul.append($li);
+  var $content = $("<div>")
+                  .addClass("info")
+                  .html("<h3>" + title + "</h3><h4>Released in: " + year + "</h4>");
+  var $detailLink = $("<a>").addClass("showDetails").html("Details")
+  var $li = $("<li>").data("id", movie["imdbID"]).addClass("movie group").append($content)
+  $li.append($detailLink)
+  this.$results.append($li);
 }
 
 $.MovieSearch.prototype.renderDetails = function (movie) {
   this.$details.empty();
-
   var $ul = $("<ul>").addClass("details");
 
   for (var property in movie) {
     if ((movie.hasOwnProperty(property)) && (property !== "Response")) {
       var $li = $("<li>").addClass("detail")
-
       if (property === "Poster") {
-        console.log("POSTER");
         var $img = $("<img>")
-                  .addClass("poster")
-                  .attr("src", movie[property])
-                  .attr("alt", "movie-poster");
+                    .addClass("poster")
+                    .attr("src", movie[property])
+                    .attr("alt", "movie-poster");
         $li.html($img);
         $ul.prepend($li);
       } else {
@@ -67,9 +75,9 @@ $.MovieSearch.prototype.renderDetails = function (movie) {
 };
 
 $.MovieSearch.prototype.detailSearch = function (e) {
-  e.preventDefault();
-  var data = { i: $(e.currentTarget).data("id") }
 
+  e.preventDefault();
+  var data = { i: $(e.currentTarget).parent().data("id") }
   $.ajax({
     method: "GET",
     url: "http://omdbapi.com/?",
@@ -77,7 +85,6 @@ $.MovieSearch.prototype.detailSearch = function (e) {
     dataType: "json",
     success: this.renderDetails.bind(this)
   });
-
 
 }
 
