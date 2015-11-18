9bc3ae7f7dca1159dfc86aeed98d27d56b9a5370
RJ Dabbar <rjdabbar@gmail.com>
2015-11-18 14:24:20 -0500

    detail view working

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ $.MovieSearch = function (el) {
   this.$el = $(el);
   this.$form = this.$el.find("form");
   this.$ul = this.$el.find("ul")
+  this.$details = this.$el.find("div#details")
   this.bindEvents();
 }
 
@@ -31,15 +32,39 @@ $.MovieSearch.prototype.renderResults = function (data) {
 };
 
 $.MovieSearch.prototype.generateResult = function (movie) {
-  var $content = $("<div>").html(movie["Title"]);
+  var title, year;
+  title = movie["Title"];
+  year = movie["Year"];
+  var $content = $("<div>").html("<h3>" + title + "</h3><h4>Released in: " + year + "</h4>");
   var $li = $("<li>").data("id", movie["imdbID"]).addClass("movie").append($content)
   this.$ul.append($li);
 }
 
-$.MovieSearch.prototype.renderDetails = function (data) {
+$.MovieSearch.prototype.renderDetails = function (movie) {
+  this.$details.empty();
 
+  var $ul = $("<ul>").addClass("details");
 
-}
+  for (var property in movie) {
+    if ((movie.hasOwnProperty(property)) && (property !== "Response")) {
+      var $li = $("<li>").addClass("detail")
+
+      if (property === "Poster") {
+        console.log("POSTER");
+        var $img = $("<img>")
+                  .addClass("poster")
+                  .attr("src", movie[property])
+                  .attr("alt", "movie-poster");
+        $li.html($img);
+        $ul.prepend($li);
+      } else {
+        $li.html("<strong>" + property + "</strong>" + ": " + movie[property]);
+        $ul.append($li);
+      }
+    }
+  }
+  this.$details.append($ul);
+};
 
 $.MovieSearch.prototype.detailSearch = function (e) {
   e.preventDefault();
